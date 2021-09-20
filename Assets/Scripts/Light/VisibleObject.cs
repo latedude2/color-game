f09c0157fb9cc1c71898107b288274e035ff4965
malteerasmussen <malteerasmussen@gmail.com>
2021-09-20 12:46:22 +0200

    Find and add shinepoints at the corners of box collider.

--- Assets/Scripts/Light/VisibleObject.cs
+++ Assets/Scripts/Light/VisibleObject.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class VisibleObject : MonoBehaviour
 {
-    private Transform[] shinePoints;    //A list of points of the box where we check if the box is hit by light
+    private Vector3[] shinePoints;    //A list of points of the box where we check if the box is hit by light
     private LightManager lightManager;
 
     void Start()
@@ -18,15 +18,43 @@ public class VisibleObject : MonoBehaviour
         //TODO: Create list of points of the cube where we check if the light is shining. 
 
         //TODO: Raycast from the lights to the points and check if any of them reach the points without being occluded. 
-        
+
+        shinePoints = FindShinePoints();
     }
 
-    Transform[] FindShinePoints()
+    Vector3[] FindShinePoints()
     {
-        List<Transform> shinepoints = new List<Transform>();
-        
-        //TODO: Create a list of points for the visible object that we will be checking for shine.
-        return shinepoints.ToArray(); 
+        //Create a list of points for the visible object that we will be checking for shine.
+        List<Vector3> shinepoints = new List<Vector3>();
+
+        BoxCollider b = GetComponent<BoxCollider>();
+
+        // Add points for each corner of the box collider to the list
+        shinepoints.Add(transform.TransformPoint(b.center + new Vector3(b.size.x, -b.size.y, b.size.z) * 0.5f));
+        shinepoints.Add(transform.TransformPoint(b.center + new Vector3(-b.size.x, -b.size.y, b.size.z) * 0.5f));
+        shinepoints.Add(transform.TransformPoint(b.center + new Vector3(-b.size.x, -b.size.y, -b.size.z) * 0.5f));
+        shinepoints.Add(transform.TransformPoint(b.center + new Vector3(b.size.x, -b.size.y, -b.size.z) * 0.5f));
+        shinepoints.Add(transform.TransformPoint(b.center + new Vector3(b.size.x, b.size.y, b.size.z) * 0.5f));
+        shinepoints.Add(transform.TransformPoint(b.center + new Vector3(-b.size.x, b.size.y, b.size.z) * 0.5f));
+        shinepoints.Add(transform.TransformPoint(b.center + new Vector3(-b.size.x, b.size.y, -b.size.z) * 0.5f));
+        shinepoints.Add(transform.TransformPoint(b.center + new Vector3(b.size.x, b.size.y, -b.size.z) * 0.5f));
+
+        return shinepoints.ToArray();
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        try
+        {
+            // Draw spheres for ShinePoints for debugging
+            foreach (Vector3 point in shinePoints)
+                Gizmos.DrawSphere(point, .05f);
+        }
+        catch
+        {
+            new UnityException();
+        }
     }
 
     GameObject[] FindPointingLights()
