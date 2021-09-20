d2d303c131bdf4068f82becea7262f04c215574d
latedude2 <simasc10@gmail.com>
2021-09-20 13:22:29 +0200

    Fix indentation

--- Assets/Scripts/Color.cs
+++ Assets/Scripts/Color.cs
@@ -1,18 +1,18 @@
 using System;
 
 namespace ColorGame{
-    
+
     [Flags]
     public enum ColorCode
     {
-        Black      = 0b_0000_0000,
-        Red    = 0b_0000_0001,
+        Black   = 0b_0000_0000,
+        Red     = 0b_0000_0001,
         Green   = 0b_0000_0010,
-        Blue = 0b_0000_0100,
-        Yellow = 0b_0000_0011,
+        Blue    = 0b_0000_0100,
+        Yellow  = 0b_0000_0011,
         Magenta = 0b_0000_0101,
-        Cyan = 0b_0000_0110,
-        White = 0b_0000_0111
+        Cyan    = 0b_0000_0110,
+        White   = 0b_0000_0111
     }
 }
 
