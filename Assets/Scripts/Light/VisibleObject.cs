f60939ed670ec8ce53b1d922715a8efc15090a36
latedude2 <simasc10@gmail.com>
2021-09-20 11:40:43 +0200

    Add function for finding lights that point towards the point

--- Assets/Scripts/Light/VisibleObject.cs
+++ Assets/Scripts/Light/VisibleObject.cs
@@ -18,7 +18,6 @@ public class VisibleObject : MonoBehaviour
         //TODO: Create list of points of the cube where we check if the light is shining. 
 
         //TODO: Raycast from the lights to the points and check if any of them reach the points without being occluded. 
-        
     }
 
     Transform[] FindShinePoints()
@@ -27,12 +26,5 @@ public class VisibleObject : MonoBehaviour
         
         //TODO: Create a list of points for the visible object that we will be checking for shine.
         return shinepoints.ToArray(); 
-    }
-
-    GameObject[] FindPointingLights()
-    {
-        //TODO: Create list of lights that are on and pointing towards the points, so we don't waste computations. 
-        //Based on the normal vectors, only three sides of a box should be lit by the same light at once. 
-        return lightManager.GetLights();
     }
 }
