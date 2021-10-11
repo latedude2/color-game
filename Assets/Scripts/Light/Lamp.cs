a4f23ede95c829737196fe68fe6a95221bf4987c
latedude2 <simasc10@gmail.com>
2021-10-11 13:28:36 +0200

    Lamp ignores player character collisions

--- Assets/Scripts/Light/Lamp.cs
+++ Assets/Scripts/Light/Lamp.cs
@@ -7,6 +7,19 @@ public class Lamp : MonoBehaviour
     [SerializeField]
     float turnSpeed = 80.0f;
 
+    private void Start() {
+        DisableCollisionWithPlayer();
+    }
+
+    private void DisableCollisionWithPlayer()
+    {
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        foreach (GameObject player in players)
+        {
+             Physics.IgnoreCollision(player.GetComponent<Collider>(), GetComponent<Collider>());
+        }
+    }
+
     public float GetTurnSpeed()
     {
         return turnSpeed;
