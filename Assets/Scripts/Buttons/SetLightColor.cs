f5c2fc11d14e60b1fb537689754188f415d05c22
Kristinn Bragi Garðarsson <kristinnbragi@gmail.com>
2021-10-13 15:50:49 +0200

    Added temporary light switch SFX

--- Assets/Scripts/Buttons/SetLightColor.cs
+++ Assets/Scripts/Buttons/SetLightColor.cs
@@ -6,9 +6,11 @@ public class SetLightColor : MonoBehaviour, Interactable
 {
     private GameObject lightGameObject;
     [SerializeField] private ColorCode lightColorToSet;
+    private AudioClip onClip;
 
     private void Start() {
         lightGameObject = transform.parent.GetComponentInChildren<Light>().gameObject;
+        onClip = (AudioClip) Resources.Load("Audio/SFX/SwitchOn");
     }
 
     public void interact()
@@ -16,5 +18,6 @@ public class SetLightColor : MonoBehaviour, Interactable
         if(!lightGameObject.activeInHierarchy)
             lightGameObject.SetActive(true);
         transform.parent.GetComponentInChildren<ColoredLight>().SetColor(lightColorToSet);
+        AudioSource.PlayClipAtPoint(onClip, GetComponent<Transform>().position);
     }
 }
