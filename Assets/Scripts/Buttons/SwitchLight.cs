4e8d7aac02a98f4d87f458f9e1bf879726ced87e
JapierniczeK <36990086+JapierniczeK@users.noreply.github.com>
2021-10-15 16:09:47 +0200

    adding animation wip

--- Assets/Scripts/Buttons/SwitchLight.cs
+++ Assets/Scripts/Buttons/SwitchLight.cs
@@ -5,12 +5,14 @@ public class SwitchLight : MonoBehaviour, Interactable
     private GameObject lightGameObject;
     private AudioClip onClip;
     private AudioClip offClip;
+    private Animation anim;
 
 
     private void Start() {
         lightGameObject = transform.parent.GetComponentInChildren<Light>().gameObject;
         onClip = (AudioClip) Resources.Load("Audio/SFX/SwitchOn");
         offClip = (AudioClip) Resources.Load("Audio/SFX/SwitchOff");
+        anim = gameObject.GetComponent<Animation>();
     }
 
     public void interact()
@@ -18,6 +20,7 @@ public class SwitchLight : MonoBehaviour, Interactable
         bool isActive = lightGameObject.activeInHierarchy;
         lightGameObject.SetActive(!isActive);
         if(isActive) {
+            anim.Play("Button Animation");
             AudioSource.PlayClipAtPoint(offClip, GetComponent<Transform>().position);
         } else {
             AudioSource.PlayClipAtPoint(onClip, GetComponent<Transform>().position);
