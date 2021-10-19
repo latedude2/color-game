31ba56fa36cee4e14a2863891956d0d060959184
JapierniczeK <36990086+JapierniczeK@users.noreply.github.com>
2021-10-19 09:55:21 +0200

    code part works

--- Assets/Scripts/Buttons/SwitchLight.cs
+++ Assets/Scripts/Buttons/SwitchLight.cs
@@ -6,13 +6,14 @@ public class SwitchLight : MonoBehaviour, Interactable
     private AudioClip onClip;
     private AudioClip offClip;
     private LightBulb lightBulb;
-
+    private Animation anim;
 
     private void Start() {
         lightGameObject = transform.parent.GetComponentInChildren<Light>().gameObject;
         lightBulb = transform.parent.GetComponentInChildren<LightBulb>();
         onClip = (AudioClip) Resources.Load("Audio/SFX/SwitchOn");
         offClip = (AudioClip) Resources.Load("Audio/SFX/SwitchOff");
+        anim = GetComponent<Animation>();
     }
 
     public void interact()
@@ -20,8 +21,10 @@ public class SwitchLight : MonoBehaviour, Interactable
         bool isActive = lightGameObject.activeInHierarchy;
         lightGameObject.SetActive(!isActive);
         lightBulb.SetActive(!isActive);
-        if(isActive) {
-            AudioSource.PlayClipAtPoint(offClip, GetComponent<Transform>().position);
+        anim["ButtonAnimation"].wrapMode = WrapMode.Once;
+        anim.Play("ButtonAnimation");
+        if (isActive) {
+            AudioSource.PlayClipAtPoint(offClip, GetComponent<Transform>().position);  
         } else {
             AudioSource.PlayClipAtPoint(onClip, GetComponent<Transform>().position);
         }
