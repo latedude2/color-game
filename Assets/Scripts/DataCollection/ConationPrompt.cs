5f9d572e18720d88f2700f130ffb3aff3b0bcbef
latedude2 <simasc10@gmail.com>
2021-11-24 21:49:28 +0100

    Add conation functionality

--- Assets/Scripts/DataCollection/ConationPrompt.cs
+++ Assets/Scripts/DataCollection/ConationPrompt.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.IO;
 
 public class ConationPrompt : MonoBehaviour
 {
     private string filename = "conation.txt";
+    private string whyFilename = "whyConation.txt";
+
+    [SerializeField] private GameObject whyConationCanvas;
     private float reportedConation = 4f; 
 
     private void Start() {
@@ -28,5 +32,28 @@ public class ConationPrompt : MonoBehaviour
         StreamWriter writer = new StreamWriter(filepath, true);
         writer.WriteLine(reportedConation.ToString());
         writer.Close();
+    }
+
+    public void SaveWhyValue()
+    {
+        string directory = System.IO.Path.Combine(Application.persistentDataPath, GameManager.Instance.session.ToString().Replace("/", "-").Replace(":", "-"));
+        if (!Directory.Exists(directory)) {
+            //if it doesn't, create it
+            Directory.CreateDirectory(directory);
+        }
+        string filepath = System.IO.Path.Combine(Application.persistentDataPath, directory, whyFilename);
+        StreamWriter writer = new StreamWriter(filepath, true);
+        
+        string comment = GameObject.Find("WhyConationText").GetComponent<Text>().text;
+
+        writer.WriteLine(comment);
+        writer.WriteLine("----------------");
+        writer.Close();
+    }
+
+    public void ShowWhyQuestion()
+    {
+        GameObject.Find("ConationCanvas").SetActive(false);
+        whyConationCanvas.SetActive(true);
     }
 }
