aef5eabc2ce42168be749c7cd2fca01f7d33e1ff
latedude2 <simasc10@gmail.com>
2022-02-21 13:23:10 +0100

    Add button to toggle disabled lights in edit mode

--- Assets/Editor/MenuItems.cs
+++ Assets/Editor/MenuItems.cs
@@ -3,10 +3,10 @@ using UnityEditor;
 
 public class MenuItems
 {
-    [MenuItem("Tools/Update colors in editor")]
-    private static void NewMenuOption()
+    static bool showDisabledLights = true;
+    [MenuItem("Enlit Games/Update colors in editor")]
+    private static void UpdateColors()
     {
-
         WireSurface[] wires = GameObject.FindObjectsOfType<WireSurface>();
         foreach(WireSurface comp in wires)
         {
@@ -17,6 +17,18 @@ public class MenuItems
         foreach(ColoredLight comp in lights)
         {
             comp.SetColorAtStart();
+        }
+        UnityEngine.SceneManagement.Scene activeScene = UnityEditor.SceneManagement.EditorSceneManager.GetActiveScene();
+        UnityEditor.SceneManagement.EditorSceneManager.MarkSceneDirty(activeScene);
+    }
+    [MenuItem("Enlit Games/Toggle showing disabled lights")]
+    private static void ToggleShowingDisabledLights()
+    {
+        showDisabledLights = !showDisabledLights;
+        ColoredLight[] lights = GameObject.FindObjectsOfType<ColoredLight>();
+        foreach(ColoredLight comp in lights)
+        {
+            comp.toggleShowingLightsInEditor(showDisabledLights);
         }
         UnityEngine.SceneManagement.Scene activeScene = UnityEditor.SceneManagement.EditorSceneManager.GetActiveScene();
         UnityEditor.SceneManagement.EditorSceneManager.MarkSceneDirty(activeScene);
