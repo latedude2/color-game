0174ed0c475e82bea2ec91bb5b4ca0a10f2ab26e
Kristinn Bragi Garðarsson <kristinnbragi@gmail.com>
2022-03-25 09:46:14 +0100

    Implemented Temporary voice acting event

---
+++ Assets/Scripts/Audio/ActivatableNarration.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using FMODUnity;
+
+public class ActivatableNarration : MonoBehaviour, Activatable
+{
+    private StudioEventEmitter _audio;
+    bool soundsEnabled = false;
+
+    void Awake() {
+        _audio = GetComponent<StudioEventEmitter>();
+    }
+
+    private void Start() {
+        // Avoid sounds from playing on new scene load
+        Invoke(nameof(EnableSounds), .2f);
+    }
+
+    public void Activate() {
+        if(soundsEnabled) {
+            _audio.Play();
+            soundsEnabled = false;
+        }
+    }
+
+    public void Deactivate() {
+        return;
+    }
+
+    void EnableSounds() {
+        soundsEnabled = true;
+    }
+}
