8b5bbca6c78a418c32efc9c5936b36a03b308dc2
latedude2 <simasc10@gmail.com>
2022-03-31 13:33:58 +0200

    Add choice manager

--- Assets/Scripts/GameManagement/EndTrigger.cs
+++ Assets/Scripts/GameManagement/EndTrigger.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class EndTrigger : MonoBehaviour
 { 
+    [Tooltip("Use this for specifying to load any other level than the next one in the build settings. Intended to be used for levels with multiple exits. ")]
     [SerializeField] string customLevelName = "";
 
+
+    [Tooltip("Example: Informing people")]
+    [SerializeField] string choiceName = "";
+
+    [Tooltip("Example: side with blue")]
+    [SerializeField] string choiceSolution = "";
+
     private void OnTriggerEnter(Collider other) {
+        if(choiceName != "" && choiceSolution != "")
+        {
+            ChoiceManager.SaveChoice(choiceName, choiceSolution);
+        }
         if (other.tag == "Player")
         {
             if(customLevelName != "")
