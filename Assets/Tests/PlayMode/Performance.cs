6cb7b435f07fddaf90443d7714190582bc0e61d8
latedude2 <simasc10@gmail.com>
2022-04-02 17:45:09 +0200

    Add performance test for level 3 in high and low quality presets

--- Assets/Tests/PlayMode/Performance.cs
+++ Assets/Tests/PlayMode/Performance.cs
@@ -1,16 +1,33 @@
 using System.Collections;
 using NUnit.Framework;
 using Unity.PerformanceTesting;
-using UnityEngine;
-using UnityEngine.Profiling;
 using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+using UnityEngine;
 
 public class Performance
 {
     [Test, Performance]
-    public void Test()
+    public void ExampleTest()
     {
         Measure.Method(Counter).Run();
+    }
+
+    [UnityTest, Performance]
+    public IEnumerator Level03()
+    {
+        SceneManager.LoadScene("Level03");
+
+        yield return Measure.Frames().WarmupCount(5).MeasurementCount(100).Run();
+    }
+
+    [UnityTest, Performance]
+    public IEnumerator Level03LowQuality()
+    {
+        SceneManager.LoadScene("Level03");
+        QualitySettings.SetQualityLevel(0, true);
+
+        yield return Measure.Frames().WarmupCount(5).MeasurementCount(100).Run();
     }
 
     private static void Counter()
