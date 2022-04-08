d9578bd2885448dea36d05f3c98b78673d404b8a
latedude2 <simasc10@gmail.com>
2022-04-08 14:48:19 +0200

    Add prefab that is attached to all UI only levels to ensure mouse control is returned to player

---
+++ Assets/Scripts/UI/ReturnMouseControl.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnMouseControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Settings.UnlockCursor();
+    }
+}
