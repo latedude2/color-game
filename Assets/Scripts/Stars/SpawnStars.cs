feeaab4c63e9a4feabf648d7a4f4d291cd7095b2
Sara886a <43111320+Sara886a@users.noreply.github.com>
2022-04-24 17:10:37 +0200

    converted to vector3 and horizontal grid direction

--- Assets/Scripts/Stars/SpawnStars.cs
+++ Assets/Scripts/Stars/SpawnStars.cs
@@ -3,16 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnStars : MonoBehaviour
+
 {
-    // Start is called before the first frame update
-    void Start()
+    public float radius = 1;
+    public float displayHeight = 20;
+    public Vector3 areaSize = new Vector3(1,1,1);
+    public int rejectionSamples = 10;
+    public float dispRadius = 1;
+
+    List<Vector3> points;
+
+    // method called when values are changed in inspector
+    void OnValidate()
     {
-        
+        // calling the function from the other script to generate a list of points
+        points = StarGeneration.GeneratePoints(radius, areaSize, rejectionSamples, displayHeight);
+
     }
 
-    // Update is called once per frame
-    void Update()
+    // drawing spheres at the points
+    // and drawing the generation area
+    void OnDrawGizmos()
     {
-        
+        //Gizmos.DrawWireCube(areaSize / 2, areaSize);
+
+        if (points != null)
+        {
+            /*Gizmos.DrawSphere(points[0], dispRadius);
+            Gizmos.DrawSphere(points[1], dispRadius);
+            Gizmos.DrawSphere(points[2], dispRadius);*/
+            foreach (Vector3 point in points)
+            {
+                Gizmos.DrawSphere(point, dispRadius);
+            }
+        }
     }
 }
--- Assets/Scripts/Stars/StarGeneration.cs
+++ Assets/Scripts/Stars/StarGeneration.cs
@@ -2,33 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StarGeneration : MonoBehaviour
+public static class StarGeneration
 {
     //generate a list of points for the stars
-    public static List<Vector2> GeneratePoint(float radius, Vector2 sampleAreaSize, int attemptBefReject = 30)
+    public static List<Vector3> GeneratePoints(float radius, Vector3 sampleAreaSize, int attemptBefReject, float dispHeight)
     {
         // each cell has a diagonal equal to the radius, so we have to calculate the side lengths
         float cellSize = radius / Mathf.Sqrt(2);
 
         // the grid will just be however many cells fit withing the size of the sample area
-        int[,] grid = new int[Mathf.CeilToInt(sampleAreaSize.x / cellSize), Mathf.CeilToInt(sampleAreaSize.y / cellSize)];
+        int[,] grid = new int[Mathf.CeilToInt(sampleAreaSize.x / cellSize), Mathf.CeilToInt(sampleAreaSize.z / cellSize)];
 
         // list with all the generated points to track the index of the point in each cell
         // since 0 is used to denote an empty cell, the index of points starts from 1
-        List<Vector2> points = new List<Vector2>();
+        List<Vector3> points = new List<Vector3>();
 
         // list of spawn points where new points can be generated around
-        List<Vector2> spawnPoints = new List<Vector2>();
+        List<Vector3> spawnPoints = new List<Vector3>();
 
         // adding a starting point to start generating around (added in the center for simplicity)
-        spawnPoints.Add(sampleAreaSize / 2);
+        Vector3 startPoint = new Vector3(sampleAreaSize.x / 2, 0, sampleAreaSize.z / 2);
+        spawnPoints.Add(startPoint);
 
         // looping through spawnpoints to try and generate new points
         while(spawnPoints.Count > 0)
         {
             // attempting to generate a new point based on a random spawnpoint in the list
             int spawnIndex = Random.Range(0, spawnPoints.Count);
-            Vector2 spawnCenter = spawnPoints[spawnIndex];
+            Vector3 spawnCenter = spawnPoints[spawnIndex];
 
             // after attemptBefReject tries to spawn a new point, if they all falied then the point will be removed as a spawnPoint
             bool candidateAccepted = false;
@@ -36,8 +37,9 @@ public class StarGeneration : MonoBehaviour
             {
                 // random point at any angle and a distance between the radius and double the radius
                 float angle = Random.value * Mathf.PI * 2;
-                Vector2 dir = new Vector2(Mathf.Sin(angle), Mathf.Cos(angle));
-                Vector2 candidate = spawnCenter + dir * Random.Range(radius, 2 * radius);
+                //float heightChange = Random.value + spawnCenter.y;  // needs to be limited in some wya maybe??
+                Vector3 dir = new Vector3(Mathf.Sin(angle), 0, Mathf.Cos(angle));
+                Vector3 candidate = spawnCenter + dir * Random.Range(radius, 2 * radius);
 
                 // checking if the new point is within another point's radius
                 if (isValid(candidate, sampleAreaSize, cellSize, points, grid, radius))
@@ -45,7 +47,7 @@ public class StarGeneration : MonoBehaviour
                     points.Add(candidate);
                     spawnPoints.Add(candidate);
                     // saving the grid position of the point
-                    grid[(int)(candidate.x / cellSize), (int)(candidate.y / cellSize)] = points.Count;
+                    grid[(int)(candidate.x / cellSize), (int)(candidate.z / cellSize)] = points.Count;
                     candidateAccepted = true;
                     break;
                 }
@@ -61,28 +63,28 @@ public class StarGeneration : MonoBehaviour
         return points;
     }
 
-    static bool isValid(Vector2 candidate, Vector2 sampleAreaSize, float cellSize, List<Vector2> points, int[,] grid, float radius)
+    static bool isValid(Vector3 candidate, Vector3 sampleAreaSize, float cellSize, List<Vector3> points, int[,] grid, float radius)
     {
         // checking tha the candidate point is within the sample area
-        if (candidate.x >=0 && candidate.x < sampleAreaSize.x && candidate.y >=0 && candidate.y < sampleAreaSize.y)
+        if (candidate.x >=0 && candidate.x < sampleAreaSize.x && candidate.z >=0 && candidate.z < sampleAreaSize.z)
         {
             //checking the cell of the candidate point
             int cellX = (int)(candidate.x / cellSize);
-            int cellY = (int)(candidate.y / cellSize);
+            int cellZ = (int)(candidate.z / cellSize);
 
             // checking any points in a 5x5 grid around the cell of the candidate
             int searchStartX = Mathf.Max(0, cellX - 2);
             int searchEndX = Mathf.Min(cellX + 2, grid.GetLength(0) - 1);
-            int searchStartY = Mathf.Max(0, cellY - 2);
-            int searchEndY = Mathf.Min(cellY + 2, grid.GetLength(1) - 1);
+            int searchStartZ = Mathf.Max(0, cellZ - 2);
+            int searchEndZ = Mathf.Min(cellZ + 2, grid.GetLength(1) - 1);
 
             // classic double for loop going through the cells in  the grid
             for (int x = searchStartX; x <= searchEndX; x++)
             {
-                for (int y = searchStartY; y <= searchEndY; y++)
+                for (int z = searchStartZ; z <= searchEndZ; z++)
                 {
                     // index of the point in the current grid cell (-1 because we count indexes from 1)
-                    int pointIndex = grid[x, y] - 1;
+                    int pointIndex = grid[x, z] - 1;
 
                     // checking that the cell is not empty
                     if (pointIndex != -1)
