952f44b47c9568d2de90a88723e92fba75c98584
Kristinn Bragi Garðarsson <kristinnbragi@gmail.com>
2022-05-08 12:04:50 +0200

    Music setup improved – loops and stuff

--- Assets/Scripts/Audio/MusicManager.cs
+++ Assets/Scripts/Audio/MusicManager.cs
@@ -18,17 +18,13 @@ public class MusicManager : MonoBehaviour {
         soundtrack = GetComponent<StudioEventEmitter>();
     }
 
+    void Start() {
+        SwitchSoundtrack(SceneManager.GetActiveScene().buildIndex);
+    }
+
     void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
-        if (scene.name.Contains("Level") && scene.name != "LevelSelect") {
-            if(scene.buildIndex < 6)
-                SwitchSoundtrack(1);
-            else if(scene.buildIndex >= 6 && scene.buildIndex < 12)
-                SwitchSoundtrack(2);
-            else if(scene.buildIndex >= 12 && scene.buildIndex < 23)
-                SwitchSoundtrack(1);
-            else if(scene.buildIndex >= 23)
-                SwitchSoundtrack(3);
-        }
+        if (scene.name.Contains("Level") && scene.name != "LevelSelect")
+            SwitchSoundtrack(scene.buildIndex);
     }
 
     public void NextSoundtrack() {
@@ -36,7 +32,14 @@ public class MusicManager : MonoBehaviour {
     }
 
     public void SwitchSoundtrack(int to) {
-        soundtrack.SetParameter("Index", to);
+        if(to < 6)
+            soundtrack.SetParameter("Index", 1);
+        else if(to >= 6 && to < 12)
+            soundtrack.SetParameter("Index", 2);
+        else if(to >= 12 && to < 23)
+            soundtrack.SetParameter("Index", 3);
+        else if(to >= 23)
+            soundtrack.SetParameter("Index", 4);
         currMusic = to;
     }
 }
