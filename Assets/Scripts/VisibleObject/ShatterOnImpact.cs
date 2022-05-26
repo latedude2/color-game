e3d38b54a279dcd05531e15dea382761563859dc
Kristinn Bragi Garðarsson <kristinnbragi@gmail.com>
2022-05-26 14:47:32 +0200

    Trigger sound on Vase collision 'Break' sound always sounds, missing non-break collision sound

--- Assets/Scripts/VisibleObject/ShatterOnImpact.cs
+++ Assets/Scripts/VisibleObject/ShatterOnImpact.cs
@@ -6,7 +6,17 @@ public class ShatterOnImpact : MonoBehaviour
 {
     private float shatterImpulse = 50f;
     [SerializeField] private GameObject replacementObject;
+    public FMODUnity.EventReference FMODEvent;
     private void OnCollisionEnter(Collision collision) {
+        // GetComponent<FMODUnity.StudioEventEmitter>().Play();
+
+        FMOD.Studio.EventInstance heal = FMODUnity.RuntimeManager.CreateInstance(FMODEvent);
+        heal.setParameterByName("Velocity Y", collision.impulse.magnitude/50);
+        Debug.Log(collision.impulse.magnitude/50);
+        heal.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject));
+        heal.start();
+        heal.release();
+
         if(collision.impulse.magnitude > shatterImpulse)
         {
             GameObject go = Instantiate(replacementObject, transform.position, transform.rotation);
