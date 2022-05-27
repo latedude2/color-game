198cfedb80bc7ec5fae487fc344a07b53ad5c892
Kristinn Bragi Garðarsson <kristinnbragi@gmail.com>
2022-05-27 12:06:08 +0200

    Updated vase collision audio now is put in its own script and has seperate events for shatter collision and non-shatter collision

--- Assets/Scripts/Audio/VaseCollisionAudio.cs
+++ Assets/Scripts/Audio/VaseCollisionAudio.cs
@@ -4,5 +4,21 @@ using UnityEngine;
 
 public class VaseCollisionAudio : MonoBehaviour
 {
-    
+    public FMODUnity.EventReference shatterEvent;
+    public FMODUnity.EventReference collideEvent;
+
+    public void PlayShatter(float force) {
+        PlayOneShot(FMODUnity.RuntimeManager.CreateInstance(shatterEvent), force);
+    }
+
+    public void PlayCollision(float force) {
+        PlayOneShot(FMODUnity.RuntimeManager.CreateInstance(collideEvent), force);
+    }
+
+    private void PlayOneShot(FMOD.Studio.EventInstance eventInstance, float force) {
+        eventInstance.setParameterByName("Velocity Y", force);
+        eventInstance.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject));
+        eventInstance.start();
+        eventInstance.release();
+    }
 }
--- Assets/Scripts/VisibleObject/ShatterOnImpact.cs
+++ Assets/Scripts/VisibleObject/ShatterOnImpact.cs
@@ -6,21 +6,21 @@ public class ShatterOnImpact : MonoBehaviour
 {
     private float shatterImpulse = 50f;
     [SerializeField] private GameObject replacementObject;
-    public FMODUnity.EventReference FMODEvent;
+    private VaseCollisionAudio audio;
+    
+    private void Start() {
+        audio = GetComponent<VaseCollisionAudio>();
+    }
+
     private void OnCollisionEnter(Collision collision) {
-        // GetComponent<FMODUnity.StudioEventEmitter>().Play();
-
-        FMOD.Studio.EventInstance heal = FMODUnity.RuntimeManager.CreateInstance(FMODEvent);
-        heal.setParameterByName("Velocity Y", collision.impulse.magnitude / shatterImpulse);
-        heal.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject));
-        heal.start();
-        heal.release();
-
         if(collision.impulse.magnitude > shatterImpulse)
         {
             GameObject go = Instantiate(replacementObject, transform.position, transform.rotation);
             go.GetComponent<Rigidbody>().velocity = GetComponent<Rigidbody>().velocity;
+            audio.PlayShatter(collision.impulse.magnitude / shatterImpulse);
             Destroy(gameObject);
+        } else {
+            audio.PlayCollision(collision.impulse.magnitude / shatterImpulse);
         }
     }
 }
