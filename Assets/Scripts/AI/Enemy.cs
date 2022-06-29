d67b84480ccf92f144587935e31a3f83e8835edc
latedude2 <simasc10@gmail.com>
2022-06-29 13:37:48 +0200

    Minor code refactor for readability

--- Assets/Scripts/AI/Enemy.cs
+++ Assets/Scripts/AI/Enemy.cs
@@ -4,43 +4,52 @@ using UnityEngine.SceneManagement;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
     private GameObject player;
-    float followSpeed = 3.5f;
+    float followSpeed = 3.5f;       // Speed of the enemy when following the player
+    float pathUpdateTime = 0.5f;    // How often the path is updated
+
+    bool frozen = true;
     void Start()
     {
-        UpdateSpeed();
-        GetComponent<VisibleObjectVisibility>().visibilityChanged.AddListener(UpdateSpeed);
+        UpdateState();
+        GetComponent<VisibleObjectVisibility>().visibilityChanged.AddListener(UpdateState);
         player = GameObject.FindGameObjectWithTag("Player");
-        InvokeRepeating("UpdatePath", 0f, 0.5f);
+        InvokeRepeating("UpdatePath", 0f, pathUpdateTime);
     }
 
     void UpdatePath(){
         GetComponent<NavMeshAgent>().SetDestination(player.transform.position);
     }
 
-    void UpdateSpeed()
+    void UpdateState()
     {
         if(GetComponent<VisibleObjectVisibility>().visible)
         {
-            GetComponent<NavMeshAgent>().speed = followSpeed;
+            Unfreeze();
         }
         else
         {
-            GetComponent<NavMeshAgent>().speed = 0;
+            Freeze();
         }
     }
 
-    private void FixedUpdate() {
-        
-    }
-
-    //On collision with player, damage player
     private void OnCollisionEnter(Collision collision) {
-        if(GetComponent<VisibleObjectVisibility>().visible && collision.gameObject.tag == "Player")
+        if(!frozen && collision.gameObject.tag == "Player")
         {
             LevelManager levelManager = GameObject.Find("LevelManager").GetComponent<LevelManager>();
             levelManager.ReloadScene();
         }
+    }
+
+    void Unfreeze()
+    {
+        frozen = false;
+        GetComponent<NavMeshAgent>().speed = followSpeed;
+    }
+
+    void Freeze()
+    {
+        frozen = true;
+        GetComponent<NavMeshAgent>().speed = 0;
     }
 }
