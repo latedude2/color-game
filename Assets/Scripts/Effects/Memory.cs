b194dc2bda749c83dbc4f275e0c4dee4b0b2e1a6
latedude2 <simasc10@gmail.com>
2022-07-11 13:46:17 +0200

    Fade memory text opacity

--- Assets/Scripts/Effects/Memory.cs
+++ Assets/Scripts/Effects/Memory.cs
@@ -7,20 +7,26 @@ using TMPro;
 public class Memory : MonoBehaviour
 {
     public float memoryDuration = 5f;
+    private float currentMemoryDuration = 0f;
     public string memoryText = "";
     Transform player;
+
+    TextMeshProUGUI text;
 
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
-        GetComponentInChildren<TextMeshProUGUI>().text = memoryText;
+        text = GetComponentInChildren<TextMeshProUGUI>();
+        text.text = memoryText;
+        currentMemoryDuration = memoryDuration;
     }
 
     void Update()
     {
         transform.LookAt(player);
-        memoryDuration -= Time.deltaTime;
-        if(memoryDuration < 0)
+        currentMemoryDuration -= Time.deltaTime;
+        text.color = new Color(text.color.r, text.color.g, text.color.b, currentMemoryDuration / memoryDuration);
+        if(currentMemoryDuration < 0)
         {
             Destroy(gameObject);
         }
